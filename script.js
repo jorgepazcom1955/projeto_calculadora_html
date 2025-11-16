b268563886eeef6312613b6e69a6f3ce7f47b291
Jorge Luiz Soares <76787525+jorgepazcom1955@users.noreply.github.com>
2025-11-16 16:43:40 -0300

    Calculadora Digital

---
+++ script.js
@@ -0,0 +1,24 @@
+function insertToDisplay(data){
+    document.querySelector('#display').value += data
+}
+ 
+function clean() {
+     document.querySelector('#display').value =''
+}
+
+function back() {
+    const display = document.querySelector('#display')
+    display.value = display.value.slice(0, -1)
+}
+
+function resultado() {
+    const display = document.querySelector('#display')
+    try {
+        display.value = eval(display.value)
+    } catch {
+        display.value = 'Erro'
+    }
+}
+ 
+
+    
